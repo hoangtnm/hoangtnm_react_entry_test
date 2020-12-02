2ba12b5aed12983b7218b73311f8677213839de4
hoangtnm <minhhoangtrannhat@gmail.com>
2020-12-02 18:57:36 +0700

    feat: JS exercises

---
+++ 2a.js
@@ -0,0 +1,18 @@
+var arr1 = [3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3];
+var counts = {};
+var maxKey = null;
+var maxValue = 0;
+
+for (var num of arr1) {
+  counts[num] = counts[num] ? counts[num] + 1 : 1;
+}
+
+for (var key of Object.keys(counts)){
+    value = counts[key];
+    if (value > maxValue){
+        maxKey = key;
+        maxValue = value;
+    }
+}
+
+console.log(`${maxKey} ( ${maxValue} times )`);
